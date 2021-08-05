9d3b50625a3296a37441d7002c68edad6c5728be
santiagoD1313 <diazipiasanti49@gmail.com>
2021-08-05 09:45:54 -0500

    hola

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          HACKERS
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.youtube.com/watch?v=KZjI8VQsUms"
           target="_blank"
           rel="noopener noreferrer"
         >
